de8f46b015b3c9e8d1ef0ac572b5f8c4a69b3330
Albéric Cohen <cohen.alberic@gmail.com>
2021-04-03 19:03:07 +0200

    Revert "preparation CI"
    This reverts commit 3eef27e0cc1ae4afd3993a0cbca6edd253410e4d.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,17 @@
 import { ApolloClient, InMemoryCache, NormalizedCacheObject } from '@apollo/client';
 import { ApolloProvider } from '@apollo/client/react';
 import React from 'react';
-import { BrowserRouter, Switch } from 'react-router-dom';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home';
+import SignUp from './components/Signup';
 import { Container } from '@material-ui/core';
-import  { authContext, useProvideAuth } from './services/AuthService';
+import  { authContext, useAuth, useProvideAuth } from './services/AuthService';
+import SignIn from './components/Signin';
+import CharacterSelect from './components/CharacterSelect';
 import Navigation from './components/Navigation';
+import Arena from './components/Arena';
+import { useUrlQuery } from './services/Utils';
 import Router from './components/Router';
 
 
